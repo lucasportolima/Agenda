3261907145ede14d6fbd211015cef577aa65b8b4
root <root@lucasporto-VirtualBox.(none)>
2017-07-03 13:49:37 -0300

    Já tá add os pacientes, tá com um bug no id de cada paciente

--- calcula-imc.js
+++ calcula-imc.js
@@ -10,11 +10,17 @@ function inserir() {
   var altura = document.getElementById("campo-altura").value;
   var imc = peso / (altura * altura);
 
-  document.getElementById('info-nome').textContent = nome;
-  document.getElementById('info-peso').textContent = peso;
-  document.getElementById('info-altura').textContent = altura;
-  document.getElementById('info-imc').textContent = imc;
+  var antigo = document.getElementById('pacientes').innerHTML;
 
+  var novo = '<tr id="paciente' + document.getElementById("pacientes").children[0].children.length + '"> <td id="info-nome">' + nome + '</td> <td id="info-peso">' + peso + '</td> <td id="info-altura">' + altura + '</td> <td id="info-imc">' + imc + '</td> </tr>'
+
+  //document.getElementById('info-nome').textContent = nome;
+  //document.getElementById('info-peso').textContent = peso;
+  //document.getElementById('info-altura').textContent = altura;
+  //document.getElementById('info-imc').textContent = imc;
+
+  var novo = antigo + novo;
+  document.getElementById('pacientes').innerHTML = novo;
 
   // joga tr aqui dentro
   //document.getElementById("pacientes").children[0].children[]
