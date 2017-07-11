37fcb6de2be7435460c77c4535d1304f998c27d4
root <root@lucasporto-VirtualBox.(none)>
2017-07-11 13:48:46 -0300

    Foi adicionado um metodo de inserir registros a partir de um json, foi criado uma pasta de teste e tbm foi criado um arquivo de estilo

--- calcula-imc.js
+++ calcula-imc.js
@@ -4,7 +4,49 @@
 
 */
 
+function view() {
+
+  // Resgatar valores.
+  view.prototype.resgatarValores = function(){
+    $('#resultado').html('Carregando dados...');
+
+    $.getJSON('Data.json', function(data){
+  
+      for (var i = 0; i < data.pacientes.length; i++){
+
+        var id = data.pacientes[i].id;
+        var nome = data.pacientes[i].nome;
+        var peso = data.pacientes[i].peso;
+        var altura = data.pacientes[i].altura;
+        var imc = peso / (altura * altura);
+
+        var antigo = document.getElementById('pacientes').innerHTML;
+
+        var novo = '<tr id="paciente' + document.getElementById("pacientes").children.length + '">' +
+                   '<td id="info-id">' + id + '</td>' +
+                   '<td id="info-nome">' + nome + '</td>' +
+                   '<td id="info-peso">' + peso + '</td>' +
+                   '<td id="info-altura">' + altura + '</td>' + 
+                   '<td id="info-imc">' + imc + '</td>' + 
+                   '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="excluir(paciente' + document.getElementById("pacientes").children.length + ');">Excluir</button> </td>' + 
+                   '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="editar(paciente' + document.getElementById("pacientes").children.length + ');">Editar</button> </td> </tr>'
+
+        var novo = antigo + novo;
+        document.getElementById('pacientes').innerHTML = novo;
+
+      }
+    });
+
+  }
+}
+
+var obj = new view();
+obj.resgatarValores();
+
+
+//Arrumar o id ao inserir
 function inserir() {
+  var id = document.getElementById("pacientes").children.length + 1;
   var nome = document.getElementById("campo-nome").value;
   var peso = document.getElementById("campo-peso").value;
   var altura = document.getElementById("campo-altura").value;
@@ -13,6 +55,7 @@ function inserir() {
   var antigo = document.getElementById('pacientes').innerHTML;
 
   var novo = '<tr id="paciente' + document.getElementById("pacientes").children.length + '">' +
+             '<td id="info-id">' + id + '</td>' +
              '<td id="info-nome">' + nome + '</td>' +
              '<td id="info-peso">' + peso + '</td>' +
              '<td id="info-altura">' + altura + '</td>' + 
@@ -25,10 +68,12 @@ function inserir() {
 
 }
 
+//Arrumar excluir, faltando excluir os tbody
 function excluir(aux) {
   aux.remove();
 }
 
+//arrumar id do editar
 function editar(aux) {
   var nome = prompt("Digite o nome:");
   var peso = prompt("Digite o peso:");
