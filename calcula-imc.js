d83bc26c92ad8cb006a56602a81973d3f2576f61
root <root@lucasporto-VirtualBox.(none)>
2017-07-13 14:04:48 -0300

    Crud está 90% terminado, falta apenas alguma verificações, foi mexido no css tbm

--- calcula-imc.js
+++ calcula-imc.js
@@ -6,45 +6,45 @@
 
 function view(aux) {
 
-    // Resgatar valores.
-    view.prototype.resgatarValores = function(){
-      $('#resultado').html('Carregando dados...');
+  // Resgatar valores.
+  view.prototype.resgatarValores = function(){
+    $('#resultado').html('Carregando dados...');
 
-      $.getJSON('Data.json', function(data){
+    $.getJSON('Data.json', function(data){
 
-        for (var i = 0; i < data.pacientes.length; i++){
+      for (var i = 0; i < data.pacientes.length; i++){
 
-          var id = data.pacientes[i].id;
-          var nome = data.pacientes[i].nome;
-          var peso = data.pacientes[i].peso;
-          var altura = data.pacientes[i].altura;
-          var imc = peso / (altura * altura);
+        var id = data.pacientes[i].id;
+        var nome = data.pacientes[i].nome;
+        var peso = data.pacientes[i].peso;
+        var altura = data.pacientes[i].altura;
+        var imc = peso / (altura * altura);
 
-          var antigo = document.getElementById('pacientes').innerHTML;
+        var antigo = document.getElementById('pacientes').innerHTML;
 
-          var novo = '<tr id="paciente' + document.getElementById("pacientes").children.length + '">' +
-                     '<td id="info-id">' + id + '</td>' +
-                     '<td id="info-nome">' + nome + '</td>' +
-                     '<td id="info-peso">' + peso + '</td>' +
-                     '<td id="info-altura">' + altura + '</td>' +
-                     '<td id="info-imc">' + imc + '</td>' +
-                     '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="excluir(paciente' + document.getElementById("pacientes").children.length + ');">Excluir</button> </td>' +
-                     '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="editar(paciente' + document.getElementById("pacientes").children.length + ');">Editar</button> </td> </tr>'
+        var novo = '<tr id="paciente' + document.getElementById("pacientes").children.length + '">' +
+        '<td id="info-id">' + id + '</td>' +
+        '<td id="info-nome">' + nome + '</td>' +
+        '<td id="info-peso">' + peso + '</td>' +
+        '<td id="info-altura">' + altura + '</td>' +
+        '<td id="info-imc">' + imc + '</td>' +
+        '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="excluir(paciente' + document.getElementById("pacientes").children.length + ');">Excluir</button> </td>' +
+        '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="editar(paciente' + document.getElementById("pacientes").children.length + ');">Editar</button> </td> </tr>'
 
-          var novo = antigo + novo;
-          document.getElementById('pacientes').innerHTML = novo;
+        var novo = antigo + novo;
+        document.getElementById('pacientes').innerHTML = novo;
 
-        }
-      });
-    }
+      }
+    });
+  }
 }
 
 var obj = new view();
 obj.resgatarValores();
 
 
-//Arrumar o id ao inserir
-function inserir() {
+// FORMS validadores
+$(document).change(function () {
   var id = document.getElementById("pacientes").children.length;
   var nome = document.getElementById("campo-nome").value;
   var peso = document.getElementById("campo-peso").value;
@@ -52,27 +52,48 @@ function inserir() {
 
   if (nome == "" || peso == "" || altura == "") {
     if (nome == ""){
-      document.getElementById("campo-nome").style["border-color"] = "red";
+      $('#campo-nome').css("border-color", "red");
+    }else{
+      $('#campo-nome').css("border-color", "transparent");
     }if (peso == ""){
-      document.getElementById("campo-peso").style["border-color"] = "red";
+      $('#campo-peso').css("border-color", "red");
+    }else{
+      $('#campo-peso').css("border-color", "transparent");
     }if (altura == ""){
-      document.getElementById("campo-altura").style["border-color"] = "red";
+      $('#campo-altura').css("border-color", "red");
+    }else{
+      $('#campo-altura').css("border-color", "transparent");
     }
-    view("limpar");
+    $('#adicionar-paciente').hide();
+  }else{
+    $('#campo-nome').css("border-color", "transparent");
+    $('#campo-peso').css("border-color", "transparent");
+    $('#campo-altura').css("border-color", "transparent");
+    $('#adicionar-paciente').show();
   }
+
+});
+
+//Arrumar o id ao inserir
+function inserir() {
+
+  var id = document.getElementById("pacientes").children.length;
+  var nome = document.getElementById("campo-nome").value;
+  var peso = document.getElementById("campo-peso").value;
+  var altura = document.getElementById("campo-altura").value;
 
   var imc = peso / (altura * altura);
 
   var antigo = document.getElementById('pacientes').innerHTML;
 
   var novo = '<tr id="paciente' + document.getElementById("pacientes").children.length + '">' +
-             '<td id="info-id">' + id + '</td>' +
-             '<td id="info-nome">' + nome + '</td>' +
-             '<td id="info-peso">' + peso + '</td>' +
-             '<td id="info-altura">' + altura + '</td>' +
-             '<td id="info-imc">' + imc + '</td>' +
-             '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="excluir(paciente' + document.getElementById("pacientes").children.length + ');">Excluir</button> </td>' +
-             '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="editar(paciente' + document.getElementById("pacientes").children.length + ');">Editar</button> </td> </tr>'
+  '<td id="info-id">' + id + '</td>' +
+  '<td id="info-nome">' + nome + '</td>' +
+  '<td id="info-peso">' + peso + '</td>' +
+  '<td id="info-altura">' + altura + '</td>' +
+  '<td id="info-imc">' + imc + '</td>' +
+  '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="excluir(paciente' + document.getElementById("pacientes").children.length + ');">Excluir</button> </td>' +
+  '<td> <button id="paciente' + document.getElementById("pacientes").children.length + '" class="botao bto-principal" onclick="editar(paciente' + document.getElementById("pacientes").children.length + ');">Editar</button> </td> </tr>'
 
   var novo = antigo + novo;
   document.getElementById('pacientes').innerHTML = novo;
@@ -88,26 +109,24 @@ function excluir(aux) {
 function editar(aux) {
   var nome = prompt("Digite o nome:");
   if (nome != null) {
-      aux.children[1].innerHTML = nome;
+    aux.children[1].innerHTML = nome;
   }
 
   var peso = prompt("Digite o peso:");
   if (peso != null) {
-      aux.children[2].innerHTML = peso;
+    aux.children[2].innerHTML = peso;
   }else {
-      peso = parseFloat(aux.children[2].innerHTML);
+    peso = parseFloat(aux.children[2].innerHTML);
   }
 
   var altura = prompt("Digite o altura:");
   if (altura != null) {
-      aux.children[3].innerHTML = altura;
+    aux.children[3].innerHTML = altura;
   }else {
-      altura = parseFloat(aux.children[3].innerHTML);
+    altura = parseFloat(aux.children[3].innerHTML);
   }
 
   aux.children[4].innerHTML = peso / (altura * altura);
-
-
 
 }
 
